b1b58dc0a31510a3fdd41ece25de2d0a0480457a
john <john@protechig.com>
2022-10-16 13:33:46 -0700

    updates

--- themes/afrixi/assets/js/main.js
+++ themes/afrixi/assets/js/main.js
@@ -54,69 +54,76 @@ document.addEventListener( 'click', function( e ) {
 
 
 //sound
-onload = function() {
-    if ('speechSynthesis' in window) with(speechSynthesis) {
+// onload = function() {
+//     if ('speechSynthesis' in window) with(speechSynthesis) {
 
 
-        var playEle = document.querySelector('#play');
-        var pauseEle = document.querySelector('#pause');
-        var stopEle = document.querySelector('#stop');
-        var flag = false;
+//         var playEle = document.querySelector('#play');
+//         var pauseEle = document.querySelector('#pause');
+//         var stopEle = document.querySelector('#stop');
+//         var flag = false;
 
 
-        playEle.addEventListener('click', onClickPlay);
-        pauseEle.addEventListener('click', onClickPause);
-        stopEle.addEventListener('click', onClickStop);
+//         playEle.addEventListener('click', onClickPlay);
+//         pauseEle.addEventListener('click', onClickPause);
+//         stopEle.addEventListener('click', onClickStop);
 
-        function onClickPlay() {
-            if(!flag){
-                flag = true;
-                utterance = new SpeechSynthesisUtterance(document.getElementById('manifestation').textContent);
-                utterance.voice = getVoices()[0];
-                utterance.onend = function(){
-                    flag = false; playEle.className = pauseEle.className = ''; stopEle.className = 'stopped';
-                };
-                playEle.className = 'played';
-                stopEle.className = '';
-                synth.speak(utterance);
-            }
-             if (paused) { /* unpause/resume narration */
-                playEle.className = 'played';
-                pauseEle.className = '';
-                resume();
-            } 
-        }
+//         function onClickPlay() {
+//             if(!flag){
+//                 flag = true;
+//                 utterance = new SpeechSynthesisUtterance(document.getElementById('manifestation').textContent);
+//                 utterance.voice = getVoices()[0];
+//                 utterance.onend = function(){
+//                     flag = false; playEle.className = pauseEle.className = ''; stopEle.className = 'stopped';
+//                 };
+//                 playEle.className = 'played';
+//                 stopEle.className = '';
+//                 synth.speak(utterance);
+//             }
+//              if (paused) { /* unpause/resume narration */
+//                 playEle.className = 'played';
+//                 pauseEle.className = '';
+//                 resume();
+//             } 
+//         }
 
-        function onClickPause() {
-            if(speaking && !paused){ /* pause narration */
-                pauseEle.className = 'paused';
-                playEle.className = '';
-                pause();
-            }
-        }
+//         function onClickPause() {
+//             if(speaking && !paused){ /* pause narration */
+//                 pauseEle.className = 'paused';
+//                 playEle.className = '';
+//                 pause();
+//             }
+//         }
 
-        function onClickStop() {
-            if(speaking){ /* stop narration */
-                /* for safari */
-                stopEle.className = 'stopped';
-                playEle.className = pauseEle.className = '';
-                flag = false;
-                cancel();
+//         function onClickStop() {
+//             if(speaking){ /* stop narration */
+//                 /* for safari */
+//                 stopEle.className = 'stopped';
+//                 playEle.className = pauseEle.className = '';
+//                 flag = false;
+//                 cancel();
 
-            }
-        }
+//             }
+//         }
 
-    }
+//     }
 
-    else { /* speech synthesis not supported */
-        msg = document.createElement('h5');
-        msg.textContent = "Detected no support for Speech Synthesis";
-        msg.style.textAlign = 'center';
-        msg.style.backgroundColor = 'red';
-        msg.style.color = 'white';
-        msg.style.marginTop = msg.style.marginBottom = 0;
-        document.body.insertBefore(msg, document.querySelector('div'));
-    }
+//     else { /* speech synthesis not supported */
+//         msg = document.createElement('h5');
+//         msg.textContent = "Detected no support for Speech Synthesis";
+//         msg.style.textAlign = 'center';
+//         msg.style.backgroundColor = 'red';
+//         msg.style.color = 'white';
+//         msg.style.marginTop = msg.style.marginBottom = 0;
+//         document.body.insertBefore(msg, document.querySelector('div'));
+//     }
 
-}
+// }
 
+playbutton.onclick = function() {
+    responsiveVoice.cancel();
+    responsiveVoice.speak(document.getElementById('text').value, document.getElementById('manifestation').value);
+  };
+  stopbutton.onclick = function() {
+    responsiveVoice.cancel();
+  };
