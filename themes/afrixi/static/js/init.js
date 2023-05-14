d1848b336fec5d1782415bc135f3a11e0334b86f
john <john@protechig.com>
2023-05-14 14:56:29 -0700

    updates

--- themes/afrixi/static/js/init.js
+++ themes/afrixi/static/js/init.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
           offset_top:80
      });
      $("#nav-side-ad").stick_in_parent({
-      offset_top:600
+     //  offset_top:600
      });
      $("#nav-side-article").stick_in_parent({
           offset_top:80
