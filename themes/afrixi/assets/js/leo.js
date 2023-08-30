8d304a258796c99863fb3cfab648a1b8b99b4d0f
john <john@protechig.com>
2023-08-30 00:25:02 -0700

    updates

--- themes/afrixi/assets/js/leo.js
+++ themes/afrixi/assets/js/leo.js
@@ -158,18 +158,6 @@ function xml_to_json_tables(url, tableid, tablediv) {
 
 
 
-
-
-
-
-    $(document).ready(function()
-{
-    $(".bio").each(function () {
-        $(this).append('<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7616051391644176" data-ad-slot="9654111001" data-ad-format="auto"></ins>');
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }); 
-});
-
   
     $(document)
       .mousemove(function (e) {
