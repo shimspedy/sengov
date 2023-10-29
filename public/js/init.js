8b4b24337158f82cdf5f36d5c9071037594a95d3
john <john@protechig.com>
2023-10-29 14:29:56 -0700

    added images

---
+++ public/js/init.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+     $('.scrollspy').scrollSpy();
+     $(".left-col-article").css("height", $(".left-col-article").parent().height());
+     $(".right-col-article").css("height", $(".right-col-article").parent().height());
+     $("#nav-side-single").stick_in_parent({
+          offset_top:80
+     });
+     $("#nav-side-ad").stick_in_parent({
+     //  offset_top:600
+     });
+     $("#nav-side-article").stick_in_parent({
+          offset_top:80
+     });
+     $("#social-icons-horizontal").stick_in_parent({
+          offset_top:$(window).height() -70,
+          spacer: false,
+     });
+     
+    $('.sidenav').sidenav();
+
+    $(".dropdown-trigger").dropdown();
+    
+    $('.fixed-action-btn').floatingActionButton();
+     
+   
+     
+});
+
+
