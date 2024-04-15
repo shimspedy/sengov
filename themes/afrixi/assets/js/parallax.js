6be0d4e4e21f80f1b931c8552df855ee4255d51e
john <36556216+shimspedy@users.noreply.github.com>
2024-04-15 18:36:09 +0000

    added aus

--- themes/afrixi/assets/js/parallax.js
+++
@@ -1,9 +0,0 @@
-// by
-// abubakersaeed.netlify.com | @AbubakerSaeed96
-// ============================================
-
-// Inspiration:
-// Tilt.js: https://gijsroge.github.io/tilt.js/
-// Andy Merskin's parallax depth cards pen: https://codepen.io/andymerskin/full/XNMWvQ/
-
-// Thank You for Viewing
